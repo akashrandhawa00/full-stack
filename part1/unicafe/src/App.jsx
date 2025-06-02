759c960bb70ecbd353b5353e7de480824c15b363
Akash Randhawa <93700117+akashrandhawa00@users.noreply.github.com>
2025-06-01 21:30:17 -0400

    Refactoring for ex 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -14,21 +14,41 @@ const Display = (props) => {
   )
 }
 
+const Statistics = ({good, neutral, bad}) => {
+  return (
+    <>
+      <h1>statistics</h1>
+    
+      <Display name='Good' value={good} />
+      <Display name='Neutral' value={neutral} />
+      <Display name='Bad' value={bad} />
+      <Display name='Total # of feedbacks' value={good + neutral + bad} />
+      <Display name='Average' value={(good*1 + bad*-1)/(good+neutral+bad)}/>
+      <Display name='Positive' value={good/(good+neutral+bad)}/>
+    </>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+
 
   const clickGood = () => {
     setGood(good + 1)
+    setTotal(total + 1)
   }
 
   const clickNeutral = () => {
     setNeutral(neutral + 1)
+    setTotal(total + 1)
   }
 
   const clickBad = () => {
     setBad(bad + 1)
+    setTotal(total + 1)
   }
 
   return (
@@ -38,10 +58,7 @@ const App = () => {
       <Button onClick={clickNeutral} text='neutral'/>
       <Button onClick={clickBad} text='bad'/>
 
-      <h1>statistics</h1>
-      <Display name='good' value={good} />
-      <Display name='neutral' value={neutral} />
-      <Display name='bad' value={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad}  />
     </div>
   )
 }
