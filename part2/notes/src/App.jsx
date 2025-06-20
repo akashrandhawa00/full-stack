8060907bba0b2472119c187e588786822964ec09
Akash Randhawa <93700117+akashrandhawa00@users.noreply.github.com>
2025-06-20 18:45:15 -0400

    Installed Axios

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Note from "./components/Note";
 
 const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
+  const [notes, setNotes] = useState(props.notes || []);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
 
